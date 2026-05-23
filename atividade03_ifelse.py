caf5dd17d2dc82eff660b3032604ae79a00721bd
joão <joaogabrielvieirasilva02@gmail.com>
2026-05-23 16:26:20 -0300

    Add files via upload

---
+++ atividade03_ifelse.py
@@ -0,0 +1,10 @@
+# autor: joão gabriel
+# projeto: condicionais 
+
+# definição das variaveis
+nome = input('digite seu nome: ')
+idade = float(input('digite sua temperatura: '))
+if idade >= 20:
+    print('temperatura maior!')
+else:
+    print('temperatura menor!')
