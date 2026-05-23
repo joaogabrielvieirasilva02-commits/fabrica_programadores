9622dc978df0e491e2dc6bf80dd650500cc594f9
joão <joaogabrielvieirasilva02@gmail.com>
2026-05-23 16:25:33 -0300

    Add files via upload

---
+++ atividade01_ifelse.py
@@ -0,0 +1,10 @@
+# autor: joão gabriel
+# projeto: condicionais 
+
+# definição das variaveis
+nome = input('digite seu nome: ')
+nota = float(input('digite sua nota: '))
+if nota >= 6:
+    print('aluno aprovado!')
+else:
+    print('aluno reprovado!')
