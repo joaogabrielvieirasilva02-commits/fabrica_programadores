accc0f38df2b993bd11532a960de2d6fb56fef09
joão <joaogabrielvieirasilva02@gmail.com>
2026-08-08 16:57:00 -0300

    Add files via upload

---
+++ web.py/preco.py
@@ -0,0 +1,14 @@
+#Altor: joao gabriel 
+#projeto ; minha primeira pagina web 
+
+#importando a biblioteca 
+import streamlit as st
+
+st.text_input('---preço dos produtos--- !!! ')
+st.text_input('entre com o custus do produtos: ')
+st.text_input('custo de mão de obra: ')
+st.text_input('custo de energia: ')
+st.text_input('custo de internet: ')
+st.text_input('custo de agua: ')
+st.text_input('imposto  5%: ')
+st.button('calcular preço de venda: ')
---
+++ web.py/web.py
@@ -0,0 +1,12 @@
+#Altor: joao gabriel 
+#projeto ; minha primeira pagina web 
+
+#importando a biblioteca 
+import streamlit as st
+
+st.write('ola mundo!!!')
+st.write('joao gabriel')
+st.write('--- Sistema de calculo de imc---')
+st.text_input('digite seu peso: ')
+st.text_input('digite sua altura ')
+st.button('calcular imc')
