28ac828bf3d22b817c4719060c329ec5ae73df5e
joão <joaogabrielvieirasilva02@gmail.com>
2026-08-15 14:56:06 -0300

    Add files via upload

--- web.py/web.py
+++ web.py/web.py
@@ -4,9 +4,24 @@
 #importando a biblioteca 
 import streamlit as st
 
-st.write('ola mundo!!!')
-st.write('joao gabriel')
-st.write('--- Sistema de calculo de imc---')
-st.text_input('digite seu peso: ')
-st.text_input('digite sua altura ')
-st.button('calcular imc')
+st.title('--- Sistema de calculo de imc---')
+peso = st.number_input('digite seu peso: ')
+altura = st.number_input('digite sua altura: ')
+if st.button('calcular imc'):
+    if peso > 0 and altura >0:
+        imc = peso / (altura ** 2)
+        st.success(f'seu imc e: {imc:.2f}!', icon="✅")
+        if imc <= 18.5:
+            st.error('abixo do peso', icon="🚨")
+        elif imc <= 24.9:
+            st.success('peso normal!', icon="✅")
+        elif imc <= 29.9:
+            st.warning('acima do peso!', icon="⚠️")
+        elif imc <= 34.9:
+            st.warning('obesidade grau I!', icon="⚠️")
+        elif imc <= 39.9:
+            st.warning('obseidade grau II!', icon="⚠️")
+        else:
+            st.error('obesidade grau III(mórbida)!', icon="🚨")
+    else:
+        st.error('digite um numero valido!', icon="🚨")
